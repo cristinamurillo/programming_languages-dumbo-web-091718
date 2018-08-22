0e878890a0dac3742899b3ff50ecdfe8414d344d
Cristina Murillo <cmurill1@jhu.edu>
2018-08-22 18:29:11 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,6 @@ require 'pry'
 def reformat_languages(languages)
   new_hash = {}
     languages.each { |style, values|
-      style_array = [style]
       values.each { |language, type_info|
         type_info.each { |type_category, type|
           new_hash[language] = {type_category => type, :style => style_array}
