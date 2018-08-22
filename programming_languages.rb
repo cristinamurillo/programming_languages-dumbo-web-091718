8137f7a86c1ea914020efc57fe3d7e578e8fa6f5
Cristina Murillo <cmurill1@jhu.edu>
2018-08-22 18:17:19 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+    languages.each { |style, values|
+      style_array = [style]
+      values.each { |language, type_info|
+        type_info.each { |type_category, type|
+          #if language.to_s == "javascript"
+          #end
+          new_hash[language] = {type_category => type, :style => style_array}
+        }
+      }
+    }
+  new_hash
 end
+
